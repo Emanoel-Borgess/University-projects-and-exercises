6ed7f366464d5adf9e47a7c51c5c5d5d84595473
Emanoel Borges <rodbemanoelstudies@gmail.com>
2025-02-15 19:54:47 -0300

    Create script.js
    Script do exercício 031.

---
+++ Programação Web I/Exercício 031/script.js
@@ -0,0 +1,6 @@
+function ocutarParagrafo() {
+    document.getElementById("paragrafos").style.display = "none";
+}
+function exibirParagrafo() {
+    document.getElementById("paragrafos").style.display = "block";
+}
